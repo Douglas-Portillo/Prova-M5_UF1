ca90d0d61bf1113c188afd5901989db426e36e04
dopoal <dopoal00@gmail.com>
2022-06-07 19:00:34 +0200

    prova

---
+++ M5UF1DouglasPortilloAlmendares/src/main/java/M5UF1/M5UF1DouglasPortilloAlmendares/HolaMon.java
@@ -0,0 +1,12 @@
+package M5UF1.M5UF1DouglasPortilloAlmendares;
+
+public class HolaMon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		System.out.print("hola món");
+		
+	}
+
+}
